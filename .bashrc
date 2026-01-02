91daf1e4f00db4646d7e3ce26d4ca3d898acac88
Dan Mihovch <dmihovch@gmail.com>
2026-01-02 17:56:41 -0500

    mod key

--- .bashrc
+++ .bashrc
@@ -43,3 +43,7 @@ alias ls='ls --color=auto'
 # export LSCOLORS="exfxcxdxbxegedabagacad"
 
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- nvim/init.lua
+++ nvim/init.lua
@@ -514,14 +514,7 @@ require('lazy').setup({
         clangd = {},
         gopls = {},
         pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+                ts_ls = {},
         --
 
         lua_ls = {
@@ -557,6 +550,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'clang-format', -- Used to format C/C++ code
+        'typescript-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
