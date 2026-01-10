a07941c24e210a0167ea6621b56e5067b280f0b3
Dan Mihovch <dmihovch@gmail.com>
2026-01-09 20:15:16 -0500

    multi monitor madness

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
+# 1. Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
+# 2. Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch your bar (replace "example" with the name in your config.ini)
-polybar --config=~/.config/polybar/config.ini dbar 2>&1 | tee -a /tmp/polybar.log & disown
+# 3. Launch bars for each monitor
+# We use xrandr to find all connected monitors and launch a bar on each.
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload dbar &
+  done
+else
+  polybar --reload dbar &
+fi
+
+echo "Bars launched..."
